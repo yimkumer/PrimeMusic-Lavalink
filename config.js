23d0b43f143735a9116a0c1b8270eb0e9ac05bc8
Yimkumer <yimkumerpongen@gmail.com>
2025-03-25 14:42:59 +0530

    Test

--- config.js
+++ config.js
@@ -1,26 +1,24 @@
-
-
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["715210330820575293", ""], 
-  mongodbUri : "mongodb+srv://yimkumerpongen:MU1UwsUU2keGqj4D@primemusic-lavalink.eash6.mongodb.net/?retryWrites=true&w=majority&appName=PrimeMusic-Lavalink",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  ownerID: ["715210330820575293", ""],
+  mongodbUri: "mongodb+srv://yimkumerpongen:MU1UwsUU2keGqj4D@primemusic-lavalink.eash6.mongodb.net/?retryWrites=true&w=majority&appName=PrimeMusic-Lavalink",
+  spotifyClientId: "",
+  spotifyClientSecret: "",
   setupFilePath: './commands/setup.json',
-  commandsDir: './commands',  
+  commandsDir: './commands',
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "YouTube Music",
+  activityType: "LISTENING",
   SupportServer: "https://discord.gg/xQF9f9yUEM",
-  embedTimeout: 5, 
-  errorLog: "", 
+  embedTimeout: 5,
+  errorLog: "",
   nodes: [
-     {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
+    {
+      name: "Khalid",
+      host: "lava-v4.ajieblogs.eu.org",
+      password: "https://dsc.gg/ajidevserver",
+      port: 80,
       secure: false
     }
   ]
